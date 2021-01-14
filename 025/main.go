106aed5a9ad84fd7630d71e196090b4787ecf952
Alexander Golovnya <algol.mail@gmail.com>
2021-01-14 22:52:45 +0700

    Add next lessons

---
+++ 025/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var x int
+var y float64
+var z int8
+
+func main() {
+	x = 42
+	y = 42.34543
+	z = 127
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", z)
+}
