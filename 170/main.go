76a85980a55dc2646115bf260535cc5029538ef5
Alexander Golovnya <algol.mail@gmail.com>
2021-02-19 22:10:58 +0700

    Add solution next exsercises

---
+++ 170/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+// write a program that
+// * launches 10 goroutines
+// * each goroutine adds 10 numbers to a channel
+// * pull the numbers off the channel and print them
+
+func main() {
+	c := make(chan int)
+
+	for j := 0; j < 10; j++ {
+		go func() {
+			for i := 0; i < 10; i++ {
+				c <- i
+			}
+		}()
+	}
+
+	fmt.Println(c)
+
+	for k := 0; k < 100; k++ {
+		fmt.Println(k, <-c)
+	}
+}
