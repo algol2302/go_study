b03e60ebad6d34128f4d25ae637b35750e90c106
Alexander Golovnya <algol.mail@gmail.com>
2021-02-27 23:28:38 +0700

    Add solution next exsercises

---
+++ 180-f/dog/dog.go
@@ -0,0 +1,8 @@
+// Package dog is an example package for golang study
+package dog
+
+// Years accepts human years and returns dog years
+func Years(years int) int {
+	dogyears := years * 7
+	return dogyears
+}
---
+++ 180-f/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/algol2302/go_study/180-f/dog"
+)
+
+func main() {
+	fmt.Println(dog.Years(5))
+}
